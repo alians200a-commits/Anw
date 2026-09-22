cb6a6656d62c82a0a54c5c7dc1fee0dc8d1c4cf4
alians200a-commits <alians200a@gmail.com>
2026-09-22 23:00:54 +0300

    ui: add selected Flaticon medical animation sources

---
+++ src/data/animatedMedicalIcons.ts
@@ -0,0 +1,10 @@
+export const MEDICAL_ANIMATED_GIFS = {
+  drugs: 'https://cdn-icons-gif.flaticon.com/6449/6449670.gif',
+  fluids: 'https://cdn-icons-gif.flaticon.com/6449/6449668.gif',
+  equipment: 'https://cdn-icons-gif.flaticon.com/9533/9533680.gif',
+  oxygen: 'https://cdn-icons-gif.flaticon.com/9859/9859811.gif',
+  stages: 'https://cdn-icons-gif.flaticon.com/15366/15366205.gif',
+  clinical: 'https://cdn-icons-gif.flaticon.com/19010/19010897.gif',
+  terms: 'https://cdn-icons-gif.flaticon.com/19010/19010897.gif',
+  abbreviations: 'https://cdn-icons-gif.flaticon.com/19010/19010897.gif'
+} as const;
