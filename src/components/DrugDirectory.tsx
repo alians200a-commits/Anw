139e330ba1eb47879936461c7cde582cc785dc1c
alians200a-commits <alians200a@gmail.com>
2026-09-22 23:32:49 +0300

    fix: remove unapproved drug animation asset

--- src/components/DrugDirectory.tsx
+++ src/components/DrugDirectory.tsx
@@ -17,7 +17,6 @@ import {
   type StackMenuItem
 } from './ui/NotificationStackMenu';
 import { AnimatedIcon } from './ui/AnimatedIcon';
-import { DALEELI_ANIMATED_ASSETS } from './ui/RemoteAnimatedAssetIcon';
 
 interface DrugDirectoryProps {
   favorites: Set<string>;
@@ -107,7 +106,6 @@ export function DrugDirectory({
         title={currentFilter}
         description="تصنيف الأدوية"
         icon={<MedicinesHealthIcon className="h-[22px] w-[22px]" />}
-        animatedIconSrc={DALEELI_ANIMATED_ASSETS.medicine}
         items={filterItems}
         selectedId={classification}
       />
