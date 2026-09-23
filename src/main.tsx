ec8b4a37ac91788209e6a5491c38f65d485c8118
alians200a-commits <alians200a@gmail.com>
2026-09-23 03:36:20 +0300

    fix: install viewport gesture guard before app render

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {installViewportGestureGuard} from './utils/installViewportGestureGuard';
 import './index.css';
 import './stages-flaticon.css';
+
+installViewportGestureGuard();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
