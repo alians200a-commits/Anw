0e43888407aecfd2f59cb26ddcbc0a470ea11482
alians200a-commits <alians200a@gmail.com>
2026-09-23 03:56:58 +0300

    feat: route /admin to secure admin console

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import AdminApp from './admin/AdminApp.tsx';
 import {installViewportGestureGuard} from './utils/installViewportGestureGuard';
 import './index.css';
 import './stages-flaticon.css';
 
 installViewportGestureGuard();
 
+const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAdminRoute ? <AdminApp /> : <App />}
   </StrictMode>,
 );
